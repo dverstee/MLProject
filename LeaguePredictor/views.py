ef4124a8ff8c0ff4385af53fc686bd759b72c757
Ralph Temperville <temperville.ralph@gmail.com>
2013-11-18 02:33:57 +0100

    Created a simple page displaying the top 3 played champions of the given summoner.

--- LeaguePredictor/views.py
+++ LeaguePredictor/views.py
@@ -1,44 +1,65 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
-from django.template import Context
 import json
-from LeaguePredictor.models import Summoner
-from pprint import pprint
 import unirest
 import logging
-import sys
+
 logger = logging.getLogger(__name__)
 
 
 def index(request):
-	print request
  	if request.method == 'GET':
-
 		return render(request, 'LeaguePredictor/index.html')
-
-
 	if request.method == 'POST':
+		name = request.POST["SummonerName"]
+		summoner_id = getAccountIdByName(name)
+		most_played_champions = None
+		if summoner_id:
+			print summoner_id
+			most_played_champions = getTopPlayedChampionsBySummonerId(summoner_id)
+		if most_played_champions:
+			my_hash = {}
+			my_hash["most_played_champions"] = parseChampionlist(most_played_champions)
+			my_hash["name"] = name
+			print my_hash["most_played_champions"]
+			return render(request, 'LeaguePredictor/info.html', my_hash)
+		else:
+			return render(request, 'LeaguePredictor/index.html')
 
+def parseChampionlist(champions):
+	parsed_list = []
+	for key, champion in champions.items():
+		champion_hash = {}
+		champion_hash["id"] = champion["championId"]
+		champion_hash["games"] = champion["totalGamesPlayed"]
+		champion_hash["image"] = "https://github.com/rwarasaurus/league-of-legends-database/blob/master/icons/%d.jpg?raw=true" % champion["championId"]
+		parsed_list.append(champion_hash)
+	return parsed_list
+def getAccountIdByName(name):
+	response = unirest.get("https://community-league-of-legends.p.mashape.com/api/v1.0/EUW/summoner/getSummonerByName/%s" % name,
+	headers={
+    	"X-Mashape-Authorization": "aa3OmkDWY3pvnQxfLQJnWa6KGdUHOPHl"
+  	});
+  	values = json.loads(response.raw_body)	
+  	if 'acctId' in values:
+  		return values['acctId']
+  	return None
 
-		response = unirest.get("https://community-league-of-legends.p.mashape.com/api/v1.0/NA/summoner/getAllPublicSummonerDataByAccount/196272",
-	  
-		headers={
-	    "X-Mashape-Authorization": "aa3OmkDWY3pvnQxfLQJnWa6KGdUHOPHl"
-	  	}
-		);
-		values = json.loads(response.raw_body)		
-		name = values['summoner']['internalName']	
-		return HttpResponse(name)
+def getInfoBySummonerId(accountId):
+   	response = unirest.get("https://community-league-of-legends.p.mashape.com/api/v1.0/EUW/summoner/getAllPublicSummonerDataByAccount/%s" % accountId,
+	headers={
+		"X-Mashape-Authorization": "aa3OmkDWY3pvnQxfLQJnWa6KGdUHOPHl"
+		});
+	values = json.loads(response.raw_body)	
+	if values:
+		return values
+	return None
 
-
-	
- 	
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse
-	
-
-
-def getInfoBySummonerName():
-   print "aa"
+def getTopPlayedChampionsBySummonerId(accountId):
+	response = unirest.get("https://community-league-of-legends.p.mashape.com/api/v1.0/EUW/summoner/retrieveTopPlayedChampions/%s" % accountId,
+	headers={
+		"X-Mashape-Authorization": "aa3OmkDWY3pvnQxfLQJnWa6KGdUHOPHl"
+		});
+	values = json.loads(response.raw_body)	
+	if values:
+		return values
+	return None
--- leagueoflegendspredictor/settings.py
+++ leagueoflegendspredictor/settings.py
@@ -51,6 +51,17 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'leagueoflegendspredictor.urls'
 
 WSGI_APPLICATION = 'leagueoflegendspredictor.wsgi.application'
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+     #'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.static',
+    'django.contrib.auth.context_processors.auth'
+)
 
 
 # Database
