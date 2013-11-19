1042df7e2f7cc870cd5e72ef3a4153eae6b685e3
accnops <arthurcnops@gmail.com>
2013-11-19 22:00:22 +0100

    home page

--- lolpredictor/urls.py
+++ lolpredictor/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'lolpredictor.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', direct_to_template, {'template': 'home.html'}),
 	url(r'^predictor/', include('lolpredictor.predictor.url', namespace='predictor')),
     url(r'^admin/', include(admin.site.urls)),
 
