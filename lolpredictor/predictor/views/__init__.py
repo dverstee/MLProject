91d41b42b86d1c1146e361e9829c874974cf3ce5
Ralph Temperville <temperville.ralph@gmail.com>
2013-12-03 21:20:41 +0100

    Some minor bug fixes

--- lolpredictor/predictor/views/__init__.py
+++ lolpredictor/predictor/views/__init__.py
@@ -1,4 +1,5 @@
 from crawler import crawler
+from crawlstats import crawlstats
 from index import index
 from neural import neural
 from search import search
--- lolpredictor/predictor/views/crawlstats.py
+++ lolpredictor/predictor/views/crawlstats.py
@@ -5,10 +5,10 @@ import globals
 
 def crawlstats(request):
     stats = {}
-    stats['nrOfSummoners'] = Summoner.object.all().count()
-    stats['nrOfBronze'] = Summoner.object.filter(tier=1).count()
-    stats['nrOfSilver'] = Summoner.object.filter(tier=2).count()
-    stats['nrOfGold'] = Summoner.object.filter(tier=3).count()
-    stats['nrOfPlatinum'] = Summoner.object.filter(tier=4).count()
-    stats['nrOfDiamond'] = Summoner.object.filter(tier=5).count()
+    stats['nrOfSummoners'] = Summoner.objects.all().count()
+    stats['nrOfBronze'] = Summoner.objects.filter(tier=1).count()
+    stats['nrOfSilver'] = Summoner.objects.filter(tier=2).count()
+    stats['nrOfGold'] = Summoner.objects.filter(tier=3).count()
+    stats['nrOfPlatinum'] = Summoner.objects.filter(tier=4).count()
+    stats['nrOfDiamond'] = Summoner.objects.filter(tier=5).count()
     return render(request, 'predictor/crawlstats.html', stats)
