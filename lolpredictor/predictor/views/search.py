2b6cb210c0f5695eb8b29b5b7f10364ef0a66fda
accnops <arthurcnops@gmail.com>
2013-12-04 13:07:27 +0100

    errors weggedaan, direct_to_template

--- lolpredictor/predictor/views/search.py
+++ lolpredictor/predictor/views/search.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
-from lolpredictor.predictor.models import Summoner
+from lolpredictor.predictor.models import Summoner, ChampionPlayed
 
 def search(request):
-    if 'srch-term' in request.GET and request.GET['srch-term']:
-        summonerName = request.GET['srch-term']
-        summoner = None
+    if 'sn' in request.GET and request.GET['sn']:
+        summonerName = request.GET['sn']
         try:
             summoner = Summoner.objects.get(name=summonerName)
         except Summoner.DoesNotExist:
-            pass
-        return render(request, 'predictor/search.html', {'summoner': summoner, 'query': summonerName})
+            return render(request, 'predictor/search.html', {'summoner': None, 'query': summonerName})
+        championPlayed = ChampionPlayed.objects.filter(summoner=summoner).order_by('-nr_gameswithchamp')
+
+        return render(request, 'predictor/search.html', {
+            'summoner': summoner,
+            'query': summonerName,
+            'championPlayed': championPlayed
+            })
     else:
         return render(request, 'predictor/search.html')
