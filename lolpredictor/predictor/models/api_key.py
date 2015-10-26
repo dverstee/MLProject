8929125749e04a0b422b6fe642a9cd63ad232115
Ralph Temperville <ralph@realo.com>
2015-10-26 21:20:05 +0100

    Make the key a primary key

--- lolpredictor/predictor/models/api_key.py
+++ lolpredictor/predictor/models/api_key.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class ApiKey(models.Model):
-    key = models.CharField(max_length=36)
+    key = models.CharField(max_length=36, primary_key=True)
     usable_from = models.DateTimeField(default=None, blank=True, null=True)
     is_in_use = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
---
+++ lolpredictor/predictor/models/task.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+class Task(models.Model):
+
+    type = models.IntegerField()
